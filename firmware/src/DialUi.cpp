43bf47dfa6310eee6fa6a370e31825eaa3ef3da9
suhmyh <suhmyh@users.noreply.github.com>
2026-08-16 23:19:13 +0800

    fix: add opaque background to all idle grid labels (fixes burn-in)

--- firmware/src/DialUi.cpp
+++ firmware/src/DialUi.cpp
@@ -190,6 +190,8 @@ void DialUi::buildMainScreen() {
     lv_obj_set_width(idleColLeft_[i], 106);
     lv_obj_set_style_text_font(idleColLeft_[i], &dsh_font_cjk_16, 0);
     lv_obj_set_style_text_color(idleColLeft_[i], kWhite, 0);
+    lv_obj_set_style_bg_color(idleColLeft_[i], kDarkBg, 0);
+    lv_obj_set_style_bg_opa(idleColLeft_[i], LV_OPA_COVER, 0);
     lv_label_set_long_mode(idleColLeft_[i], LV_LABEL_LONG_DOT);
     lv_obj_set_style_text_align(idleColLeft_[i], LV_TEXT_ALIGN_RIGHT, 0);
     lv_label_set_text(idleColLeft_[i], "");
@@ -200,6 +202,8 @@ void DialUi::buildMainScreen() {
     lv_obj_set_width(idleColRight_[i], 106);
     lv_obj_set_style_text_font(idleColRight_[i], &dsh_font_cjk_16, 0);
     lv_obj_set_style_text_color(idleColRight_[i], kWhite, 0);
+    lv_obj_set_style_bg_color(idleColRight_[i], kDarkBg, 0);
+    lv_obj_set_style_bg_opa(idleColRight_[i], LV_OPA_COVER, 0);
     lv_label_set_long_mode(idleColRight_[i], LV_LABEL_LONG_DOT);
     lv_obj_set_style_text_align(idleColRight_[i], LV_TEXT_ALIGN_LEFT, 0);
     lv_label_set_text(idleColRight_[i], "");
@@ -216,6 +220,8 @@ void DialUi::buildMainScreen() {
     lv_obj_set_width(idleNameLeft_[i], 106);
     lv_obj_set_style_text_font(idleNameLeft_[i], &dsh_font_cjk_16, 0);
     lv_obj_set_style_text_color(idleNameLeft_[i], kGray, 0);
+    lv_obj_set_style_bg_color(idleNameLeft_[i], kDarkBg, 0);
+    lv_obj_set_style_bg_opa(idleNameLeft_[i], LV_OPA_COVER, 0);
     lv_label_set_long_mode(idleNameLeft_[i], LV_LABEL_LONG_DOT);
     lv_obj_set_style_text_align(idleNameLeft_[i], LV_TEXT_ALIGN_RIGHT, 0);
     lv_label_set_text(idleNameLeft_[i], leftNames[i]);
@@ -227,6 +233,8 @@ void DialUi::buildMainScreen() {
     lv_obj_set_width(idleNameRight_[i], 106);
     lv_obj_set_style_text_font(idleNameRight_[i], &dsh_font_cjk_16, 0);
     lv_obj_set_style_text_color(idleNameRight_[i], kGray, 0);
+    lv_obj_set_style_bg_color(idleNameRight_[i], kDarkBg, 0);
+    lv_obj_set_style_bg_opa(idleNameRight_[i], LV_OPA_COVER, 0);
     lv_label_set_long_mode(idleNameRight_[i], LV_LABEL_LONG_DOT);
     lv_obj_set_style_text_align(idleNameRight_[i], LV_TEXT_ALIGN_LEFT, 0);
     lv_label_set_text(idleNameRight_[i], rightNames[i]);
